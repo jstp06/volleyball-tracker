64ec17f755c6a1aec367678bde71eaf7a5d2fca6
jstp06 <jarekstpascua@gmail.com>
2026-07-15 15:01:08 -0700

    set up express backend and react frontend, connected via CORS

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001; 
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+})); 
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'hello from server' });
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}');
+});
