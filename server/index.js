82b9b44ba6c3e4853ccaf8511d20a23eaebcdb4e
jstp06 <jarekstpascua@gmail.com>
2026-07-16 16:20:46 -0700

    added /api/matches route querying postgresql

--- server/index.js
+++ server/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 3001; 
+const pool = require('./db');
 
 app.use(cors({
     origin: 'http://localhost:5173'
 })); 
 
-
+app.get('/api/matches', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM match');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: 'Something went wrong' });
+    }
+    });
+    
 app.get('/api/hello', (req, res) => {
     res.json({ message: 'hello from server' });
 });
